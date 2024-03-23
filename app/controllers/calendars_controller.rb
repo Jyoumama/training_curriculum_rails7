6d0feacc65e066b38a935530aedd53e889d69d6b
Jyoumama <juri19771211@gmail.com>
2024-03-23 18:40:52 +0900

    Issue4,5,6の再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -13,7 +13,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars_path).permit(:date, :plan)
+    params.require(:plan,).permit(:date, :plan)
   end
 
   def getWeek
